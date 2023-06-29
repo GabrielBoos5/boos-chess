df1c348330114e83c95d5e6fc66ef297f4a0aadc
GabrielBoos5 <gduarte@vayon.com.br>
2023-06-29 17:51:16 -0300

    ranking funcionando
    Ranking 100% funcional, utilizando Bubble Sort para ordenar o array de porcentagem de acertos de todo usuário e estilizado na página

--- projeto individual chess/src/models/rankingModel.js
+++ projeto individual chess/src/models/rankingModel.js
@@ -4,7 +4,7 @@ function listar() {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
     var instrucao = `
     SELECT sum(acertos) AS 'Acertos', sum(erros) AS 'Erros', usuario.username,
-    sum(acertos) / (sum(acertos) + sum(erros)) AS 'Maior Porcentagem'
+    sum(acertos) / (sum(acertos) + sum(erros)) AS 'porcentagem'
     FROM respostas JOIN usuario ON respostas.fkUsuario = usuario.idUsuario 
     JOIN quizes ON respostas.fkQuiz = quizes.idQuiz GROUP BY usuario.username ORDER BY count(usuario.idUsuario) 
     DESC;
